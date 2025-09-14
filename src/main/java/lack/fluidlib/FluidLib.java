d087a97da0830673e600397a5b1013604d442c7f
lando <landongg7@gmail.com>
2025-09-14 12:48:03 -0600

    a

--- src/main/java/lack/fluidlib/FluidLib.java
+++ src/main/java/lack/fluidlib/FluidLib.java
@@ -1,6 +1,5 @@
 package lack.fluidlib;
 
-import lack.fluidlib.example.FluidExample;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,7 +18,7 @@ public class FluidLib implements ModInitializer {
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
 
-    //todo
+        //todo
 //        FluidExample.init();
 
     }
--- src/main/java/lack/fluidlib/FluidLibClient.java
+++ src/main/java/lack/fluidlib/FluidLibClient.java
@@ -1,7 +1,5 @@
 package lack.fluidlib;
 
-import lack.fluidlib.example.FluidExample;
-import lack.fluidlib.example.FluidExampleClient;
 import net.fabricmc.api.ClientModInitializer;
 
 public class FluidLibClient implements ClientModInitializer {
